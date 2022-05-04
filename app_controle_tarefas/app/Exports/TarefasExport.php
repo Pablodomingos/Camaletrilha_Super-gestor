261c408585f1cee7a0757ac80632fb66dfe75898
Pablo Domingos <pablodomingos1700@gmail.com>
2022-05-04 09:43:31 -0300

    feat: Adição do Export TarefasExport.

---
+++ app_controle_tarefas/app/Exports/TarefasExport.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Tarefa;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return auth()->user()->tarefas()->get();
+        // return Tarefa::all();
+    }
+
+    public function headings(): array //Declarando o tipo de retorno
+    {
+        return [
+            'ID da tarefa',
+            // 'ID do usuário',
+            // 'Data de Criação',
+            // 'Data de atualização',
+            'Tarefa',
+            'Data limite de conclsão'
+        ];
+    }
+
+    public function map($row): array
+    {
+        return [
+            $row->id,
+            $row->tarefa,
+            date('d/m/Y', strtotime($row->data_limite)),
+        ];
+    }
+}
