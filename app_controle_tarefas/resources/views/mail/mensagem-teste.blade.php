bbcffdef2d7004ea44f91f8278d3f1f0bdb2c98a
Pablo Domingos <pablodomingos1700@gmail.com>
2022-05-04 10:01:03 -0300

    feat: Criação da mensagem de teste que vai ser enviada por email para o usuário.

---
+++ app_controle_tarefas/resources/views/mail/mensagem-teste.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Introdução
+
+Logo da Adapti e inspirado no Linux SUSE ?
+
+@component('mail::button', ['url' => ''])
+Botão SUSE
+@endcomponent
+
+Um obrigado, de<br>
+{{ config('app.name') }}
+@endcomponent
